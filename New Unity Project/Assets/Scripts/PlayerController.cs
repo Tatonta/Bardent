36ac29552417d95321099e41dfa65444375b2fd7
Tatonta <69384305+Tatonta@users.noreply.github.com>
2020-08-08 15:15:56 +0200

    Update PlayerController.cs

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float movementInputDirection;
 
     private Rigidbody2D rb;
+
+    public float movementSpeed = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckInput();
+    }
+
+    /* fixed update viene usato per funzioni legate alla fisica, infatti il Fixed Update si refresha 
+        con la stessa velocità del Physics Engine di Unity */
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+
+    private void CheckInput()
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal"); //ritorna 1 se vado a destra (cioè premo D), se vado a sinistra (cioè premo A) ritorna 1
+    }
+
+    private void ApplyMovement()
+    {
+        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
     }
 }
